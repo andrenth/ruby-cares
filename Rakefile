1fd9448a43f22f6fa32a852fe9e91559d92738dd
David Albert <davidbalbert@gmail.com>
2011-12-01 14:47:58 -0500

    rake compile and rake clean work properly

--- Rakefile
+++ Rakefile
@@ -3,7 +3,13 @@ require 'rake/extensiontask'
 
 Rake::ExtensionTask.new('cares') do |ext|
   ext.lib_dir = "lib/cares"
-  ext.ext_dir = "ext/c-ares"
+  ext.ext_dir = "ext"
+
+  CLEAN.include "#{ext.ext_dir}/ares*.{c,h}"
+  CLEAN.include "#{ext.ext_dir}/bitncmp.{c,h}"
+  CLEAN.include "#{ext.ext_dir}/inet*.{c,h}"
+  CLEAN.include "#{ext.ext_dir}/*win32.h"
+  CLEAN.include "#{ext.ext_dir}/{nameser.h,setup_once.h,windows_port.c}"
 end
 
 desc "load ruby-cares in a pry or irb session (alias `rake c`)"
--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,13 +1,16 @@
 require 'mkmf'
 require 'fileutils'
 
-to_exclude = Dir["c-ares/{acountry.c,adig.c,ahost.c,config-dos.h}"]
-FileUtils.cp_r(Dir["c-ares/*.{c,h}"] - to_exclude, File.expand_path("../", __FILE__))
+to_copy = Dir[File.expand_path("../c-ares/*.{c,h}", __FILE__)]
+to_exclude = Dir[File.expand_path("../c-ares/{acountry.c,adig.c,ahost.c,config-dos.h}", __FILE__)]
+FileUtils.cp_r(to_copy - to_exclude, File.expand_path("../", __FILE__))
 
-system("patch -p1 <ares.diff")
-system("patch -p1 <ares_setup.diff")
+Dir.chdir(File.expand_path("../", __FILE__)) do
+  system("patch -p1 <ares.diff")
+  system("patch -p1 <ares_setup.diff")
+end
+
 uname = `uname -s`.chomp.downcase
-
 find_header("ares_config.h", File.expand_path("../config_#{uname}", __FILE__))
 $defs.push "-DHAVE_CONFIG_H"
 create_makefile('cares/cares')
